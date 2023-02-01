2d21a68b95bace15d5cdc307759ac88992c79033
Shyam <t7bhsh00@students.oamk.fi>
2023-02-01 23:45:11 +0200

    Countries component updated for error message

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -4,32 +4,51 @@ import React, { useState, useEffect } from "react";
 const AllCountries = () => {
    
   const [countries, setCountries] = useState([])
-  
+    const [ isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(' ')
+ 
   useEffect(() => {
     const getCountries = async () => {
-      
+      try {
         const res = await fetch("https://restcountries.com/v3.1/all")
         const data = await res.json()
         console.log(data);
         setCountries(data);
+        setIsLoading(false); 
+
+         } 
+      catch (error) {
+        setIsLoading(false);
+        setError(error.message);
+      }
     };
   getCountries();
   }, []);
 
   return (
-     <>
-       {countries.map(country => (
-         
-            <div className = "country_data">
-                <img src={country.flags.png} />
-                <h3>{country.name.common}</h3>
-                <h4> Population: {country.population}</h4>
-                <h5> Region: {country.region}</h5>
+   <div className= "component">
+     <div className="country_top"></div>
+
+    <div className="country_bottom">
+        {isLoading && !error && <h4> Loading...</h4>}
+        {error && !isLoading && <h4>{error}</h4>}
+    {
+       countries?.map(country => (
+         <div className="country_card">
+            <div className="country_img">
+               <img src={country.flags.png} alt = "" />
             </div>
 
-        ))}
-    </>
-    
+             <div className = "country_data">
+                <h3>{country.name.common}</h3>
+                <h6> Population: {country.population}</h6>
+                <h6> Region: {country.region}</h6>
+              </div>
+          </div>
+        ))
+    }
+    </div>   
+   </div> 
   );
 }
 export default AllCountries;
